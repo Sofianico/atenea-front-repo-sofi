a14bb0ddbd90f963e22a640baaa92db0937a78b6
Sofia Nicoletti <48844465+Sofianico@users.noreply.github.com>
2025-07-04 19:43:29 -0300

    Agrego archivos

---
+++ AteneaFront/next.config.mjs
@@ -0,0 +1,13 @@
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+}
+
+export default nextConfig;
